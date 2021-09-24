3ccd406bb782f27eddf0987e639ca61679ce9948
seyyed ali hossein nezhad <alihosseinnezhad3@gmail.com>
2021-09-24 17:07:31 +0330

    -Quiz pages Added

--- app/src/main/java/com/example/piston/MainActivity.kt
+++ app/src/main/java/com/example/piston/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.piston
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavType
@@ -22,22 +19,26 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.room.Room
 import com.example.data.*
 import com.example.piston.ui.theme.ReadingPage
 import com.google.accompanist.pager.ExperimentalPagerApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
 
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: ViewModel by viewModels()
+    val viewModel: ViewModel by viewModels()
+
 
 
     @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewModel.getTheoryListFromDb()
-        viewModel.getPracticalListFromDb()
-
+        viewModel.getListFromDb()
 
         setContent {
             Ui()
@@ -50,55 +51,37 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun Ui() {
         val navController = rememberNavController()
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-
         Scaffold(
-
             bottomBar = {
-                if (
-                    navBackStackEntry?.destination?.route == "Home" ||
-                    navBackStackEntry?.destination?.route == "Lessons" ||
-                    navBackStackEntry?.destination?.route == "Quizes" ||
-                    navBackStackEntry?.destination?.route == "More"
-                ) {
-                    BottomNavigation {
-                        val currentDestination = navBackStackEntry?.destination
+                BottomNavigation {
+                    val navBackStackEntry by navController
+                        .currentBackStackEntryAsState()
+                    val currentDestination = navBackStackEntry?.destination
 
-                        Constants.BottomNavigationItems.forEach { screen ->
-                            BottomNavigationItem(
-                                modifier = Modifier.background(colorResource(id = R.color.white_deep)),
-                                icon = {
-                                    Icon(
-                                        ImageVector.vectorResource(id = screen.icon),
-                                        contentDescription = null,
-                                        modifier = Modifier.size(60.dp)
-                                    )
-                                },
-                                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                                onClick = {
-                                    navController.navigate(screen.route) {
+                    Constants.BottomNavigationItems.forEach { screen ->
+                        BottomNavigationItem(
+                            icon = { Icon(screen.icon, contentDescription = null) },
+                            label = { Text(screen.title) },
+                            selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                            onClick = {
+                                navController.navigate(screen.route) {
 
-                                        popUpTo(navController.graph.findStartDestination().id) {
-                                            saveState = true
-                                        }
-
-                                        launchSingleTop = true
-
-                                        restoreState = true
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
                                     }
-                                },
-                                selectedContentColor = colorResource(id = R.color.light_blue),
-                                unselectedContentColor = colorResource(id = R.color.light_gray)
 
-                            )
-                        }
+                                    launchSingleTop = true
+
+                                    restoreState = true
+                                }
+                            }
+                        )
                     }
                 }
             }
-
         ) {
             NavHost(navController = navController, startDestination = Screen.Home.route) {
-                composable(Screen.Home.route) { Home() }
+                composable(Screen.Home.route) { Home(navController) }
                 composable(Screen.Lessons.route) { Lessons(navController) }
                 composable(Screen.Quizes.route) { Quizes() }
                 composable(Screen.More.route) { More(navController) }
@@ -108,54 +91,27 @@ class MainActivity : ComponentActivity() {
                         type = NavType.StringType
                     })
                 ) { navBackStackEntry ->
-                    if (navBackStackEntry.arguments?.getString("courses_name") == "لیست دوره های تئوری") {
-                        LessonsList(
-                            navController,
-                            navBackStackEntry.arguments?.getString("courses_name"),
-                            viewModel.theory_list
-                        )
-                    }
-                    if (navBackStackEntry.arguments?.getString("courses_name") == "لیست دوره های عملی") {
-                        LessonsList(
-                            navController,
-                            navBackStackEntry.arguments?.getString("courses_name"),
-                            viewModel.practical_list
-                        )
-                    }
-
+                    PracticalLesson(
+                        navController,
+                        navBackStackEntry.arguments?.getString("courses_name"),
+                        viewModel.list
+                    )
                 }
                 composable(
-                    "reading_page/{index}/{type}",
+                    "reading_page/{index}",
                     arguments = listOf(navArgument("index") {
                         type = NavType.IntType
-                    },
-                        navArgument("type") {
-                            type = NavType.IntType
-                        })
+                    })
                 ) {
-                    if (it.arguments?.getInt("type") == 3) {
-                        ReadingPage(
-                            navController,
-                            it.arguments!!.getInt("index"),
-                            viewModel.theory_list
 
-                        )
-                    }
-                    if (it.arguments?.getInt("type") == 7) {
-                        ReadingPage(
-                            navController,
-                            it.arguments!!.getInt("index"),
-                            viewModel.practical_list
-                        )
-                    }
-
+                    ReadingPage(navController, it.arguments!!.getInt("index"), viewModel.list)
                 }
             }
         }
     }
 
     @Composable
-    fun Home() {
+    fun Home(navController: NavController) {
         Text(text = "home")
     }
 
