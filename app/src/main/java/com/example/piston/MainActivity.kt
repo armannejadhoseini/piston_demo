6b61e9eabc906400061adbddb8b0290cd3c09855
seyyed ali hossein nezhad <alihosseinnezhad3@gmail.com>
2021-09-28 13:07:40 +0300

    - update

--- app/src/main/java/com/example/piston/MainActivity.kt
+++ app/src/main/java/com/example/piston/MainActivity.kt
@@ -5,9 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -24,14 +31,15 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 
 
 class MainActivity : ComponentActivity() {
-    val viewModel: ViewModel by viewModels()
+    private val viewModel: ViewModel by viewModels()
 
 
-
-    @ExperimentalFoundationApi
     @ExperimentalPagerApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        viewModel.getTheoryListFromDb()
+        viewModel.getPracticalListFromDb()
 
         setContent {
             Ui()
@@ -40,44 +48,59 @@ class MainActivity : ComponentActivity() {
     }
 
 
-    @ExperimentalFoundationApi
     @ExperimentalPagerApi
     @Composable
     fun Ui() {
         val navController = rememberNavController()
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+
         Scaffold(
+
             bottomBar = {
-                BottomNavigation {
-                    val navBackStackEntry by navController
-                        .currentBackStackEntryAsState()
-                    val currentDestination = navBackStackEntry?.destination
+                if (
+                    navBackStackEntry?.destination?.route == "Home" ||
+                    navBackStackEntry?.destination?.route == "Lessons" ||
+                    navBackStackEntry?.destination?.route == "Quizes" ||
+                    navBackStackEntry?.destination?.route == "More"
+                ) {
+                    BottomNavigation {
+                        val currentDestination = navBackStackEntry?.destination
 
-                    Constants.BottomNavigationItems.forEach { screen ->
-                        BottomNavigationItem(
-                            icon = {
-//                                Icon(screen.icon, contentDescription = null)
-                                   },
-                            label = { Text(screen.title) },
-                            selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                            onClick = {
-                                navController.navigate(screen.route) {
+                        Constants.BottomNavigationItems.forEach { screen ->
+                            BottomNavigationItem(
+                                modifier = Modifier.background(colorResource(id = R.color.white_deep)),
+                                icon = {
+                                    Icon(
+                                        ImageVector.vectorResource(id = screen.icon),
+                                        contentDescription = null,
+                                        modifier = Modifier.size(60.dp)
+                                    )
+                                },
+                                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                                onClick = {
+                                    navController.navigate(screen.route) {
 
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+
+                                        launchSingleTop = true
+
+                                        restoreState = true
                                     }
+                                },
+                                selectedContentColor = colorResource(id = R.color.light_blue),
+                                unselectedContentColor = colorResource(id = R.color.light_gray)
 
-                                    launchSingleTop = true
-
-                                    restoreState = true
-                                }
-                            }
-                        )
+                            )
+                        }
                     }
                 }
             }
+
         ) {
             NavHost(navController = navController, startDestination = Screen.Home.route) {
-                composable(Screen.Home.route) { Home(navController) }
+                composable(Screen.Home.route) { Home() }
                 composable(Screen.Lessons.route) { Lessons(navController) }
                 composable(Screen.Quizes.route) { Quizes() }
                 composable(Screen.More.route) { More(navController) }
@@ -87,34 +110,59 @@ class MainActivity : ComponentActivity() {
                         type = NavType.StringType
                     })
                 ) { navBackStackEntry ->
-//                    PracticalLesson(
-//                        navController,
-//                        navBackStackEntry.arguments?.getString("courses_name"),
-//                        viewModel.list
-//                    )
+                    if (navBackStackEntry.arguments?.getString("courses_name") == "لیست دوره های تئوری") {
+                        LessonsList(
+                            navController,
+                            navBackStackEntry.arguments?.getString("courses_name"),
+                            viewModel.theory_list
+                        )
+                    }
+                    if (navBackStackEntry.arguments?.getString("courses_name") == "لیست دوره های عملی") {
+                        LessonsList(
+                            navController,
+                            navBackStackEntry.arguments?.getString("courses_name"),
+                            viewModel.practical_list
+                        )
+                    }
+
                 }
                 composable(
-                    "reading_page/{index}",
+                    "reading_page/{index}/{type}",
                     arguments = listOf(navArgument("index") {
                         type = NavType.IntType
-                    })
+                    },
+                        navArgument("type") {
+                            type = NavType.IntType
+                        })
                 ) {
-//                    ReadingPage(navController, it.arguments!!.getInt("index"), viewModel.list)
+                    if (it.arguments?.getInt("type") == 3) {
+                        ReadingPage(
+                            navController,
+                            it.arguments!!.getInt("index"),
+                            viewModel.theory_list
+                        )
+                    }
+                    if (it.arguments?.getInt("type") == 7) {
+                        ReadingPage(
+                            navController,
+                            it.arguments!!.getInt("index"),
+                            viewModel.practical_list,
+                        )
+                    }
+
                 }
             }
         }
     }
 
     @Composable
-    fun Home(navController: NavController) {
+    fun Home() {
         Text(text = "home")
     }
 
-    @ExperimentalPagerApi
-    @ExperimentalFoundationApi
     @Composable
     fun Quizes() {
-        QuizPage()
+        Text(text = "quizes")
     }
 
 
