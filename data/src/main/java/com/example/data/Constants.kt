64289c1a1adac36b00d5b18cf4cf8422294a15dc
seyyed ali hossein nezhad <alihosseinnezhad3@gmail.com>
2021-09-29 08:51:47 +0300

    - update

--- data/src/main/java/com/example/data/Constants.kt
+++ data/src/main/java/com/example/data/Constants.kt
@@ -10,6 +10,7 @@ object Constants {
         Screen.More
     )
     val reading_list = listOf(1, 2, 3, 4, 5, 6, 7, 8)
-
+    val quizRange = IntRange(start = 1,endInclusive = 10)
+    val examRange = IntRange(start = 1,endInclusive = 20)
 
 }
